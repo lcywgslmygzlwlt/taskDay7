028f015cd31e1708217bd0cf70333e6a4b479853
lcydr <1211753823@qq.com>
2022-07-06 21:24:31 +0800

    组件缓存

---
+++ src/components/02/UserInfo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <span>人生格言:</span>
+      <input type="text" />
+    </div>
+    <div>
+      <span>个人简介:</span>
+      <textarea></textarea>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  created() {
+    console.log("created--userinfo");
+  },
+  destroyed() {
+    console.log("destroyed--userinfo");
+  },
+  activated() {
+    //激活
+    console.log("activated--userinfo");
+  },
+  deactivated() {
+    //失去 激活
+    console.log("deactivated--userinfo");
+  },
+};
+</script>
---
+++ src/components/02/UserName.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div>
+      <span>用户名:</span>
+      <input type="text" />
+    </div>
+    <div>
+      <span>密码:</span>
+      <input type="password" />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  created() {
+    console.log("created--userName");
+  },
+  destroyed() {
+    console.log("destroyed--userName");
+  },
+  activated() {
+    //激活
+    console.log("activated--userName");
+  },
+  deactivated() {
+    //失去 激活
+    console.log("deactivated--userName");
+  },
+};
+</script>

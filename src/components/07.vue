8a2c57d7a6a1c4398c97744338ecd968d055a23d
wgs66 <707205590@qq.com>
2022-07-06 21:23:29 +0800

    第二题

---
+++ src/components/07.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="isAbout">
+    <UseTab>
+      <template v-slot:id="scope">
+        <input type="text" v-model="scope.row.id" />
+      </template>
+      <template v-slot:name="scope">
+        <input type="text" v-model="scope.row.name" />
+      </template>
+      <template v-slot:age="scope">
+        <input type="text" v-model="scope.row.age" />
+      </template>
+      <template v-slot:tx="scope">
+        <div v-if="scope.row.type == 0">
+          <a :href="scope.row.headImgUrl">{{ scope.row.headImgUrl }}</a>
+        </div>
+        <div v-if="scope.row.type == 1">
+          <img :src="scope.row.headImgUrl" alt="" />
+        </div>
+        <div v-if="scope.row.type == 2">
+          {{ scope.row.headImgUrl }}
+        </div>
+      </template>
+    </UseTab>
+  </div>
+</template>
+
+<script>
+import UseTab from '../components/07_UseTabley.vue';
+export default {
+  name: 'isAbout',
+  components: {
+    UseTab,
+  },
+};
+</script>
